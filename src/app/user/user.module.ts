39b3e7c209b81d26f19ce3038a7f4d1b1abad885
oleh-shev <oshevchyck@gmail.com>
2020-03-21 21:56:34 +0200

    Changed Structure

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,55 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UserComponent } from './user.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UserComponent} from './user.component';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {MatButtonModule, MatListModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
-
+const routes: Routes = [
+  /*{
+    path: '', component: UserComponent,
+    children: [
+      /!*{
+        path: '', redirectTo: 'home', pathMatch: 'full'
+      },*!/
+      {
+        path: 'home', component: HomeComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'sign-up', component: SignUpComponent
+      }
+    ]
+  },*/
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'sign-up', component: SignUpComponent
+  }
+];
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, LoginComponent, SignUpComponent, HomeComponent, ToolbarComponent],
+  exports: [
+    UserComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatMenuModule,
   ]
 })
-export class UserModule { }
+export class UserModule {
+}
