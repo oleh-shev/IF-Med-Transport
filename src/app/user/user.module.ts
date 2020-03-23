ecce8687b1b827d8b56b2a9f67fe7725a1148ce7
yurakhomitsky <yura.khomitsky8@gmail.com>
2020-03-23 15:21:31 +0200

    init component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,38 +1,34 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {UserComponent} from './user.component';
-import {LoginComponent} from './login/login.component';
-import {SignUpComponent} from './sign-up/sign-up.component';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {ToolbarComponent} from './toolbar/toolbar.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/material-module/material.module';
+import { UserProfileComponent } from './user-profile/user-profile/user-profile.component';
 
 const routes: Routes = [
-  {
-    path: 'home', component: HomeComponent
-  },
-  {
-    path: 'login', component: LoginComponent
-  },
-  {
-    path: 'sign-up', component: SignUpComponent
-  }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent }
 ];
 
 @NgModule({
-  declarations: [UserComponent, LoginComponent, SignUpComponent, HomeComponent, ToolbarComponent],
+  declarations: [UserComponent, LoginComponent, SignUpComponent, HomeComponent, ToolbarComponent, UserProfileComponent],
   exports: [
     UserComponent
   ],
-    imports: [
-        CommonModule,
-        RouterModule.forChild(routes),
-        FormsModule,
-        ReactiveFormsModule,
-        MaterialModule
-    ]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ]
 })
 export class UserModule {
 }
